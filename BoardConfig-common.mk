011afa14fd85649cf02d4082fadc0c3b900d3886
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-10 09:01:12 +0800

    gs201: disable CPU mitigations
    Change-Id: Ib4a49b8253f40122dbdc2aec7cc83235e536fd45

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -51,6 +51,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=1024
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
 BOARD_KERNEL_CMDLINE += disable_dma32=on
 BOARD_KERNEL_CMDLINE += irqaffinity=0-3
+BOARD_KERNEL_CMDLINE += mitigations=off
 BOARD_BOOTCONFIG += androidboot.boot_devices=14700000.ufs
 
 TARGET_NO_BOOTLOADER := true
