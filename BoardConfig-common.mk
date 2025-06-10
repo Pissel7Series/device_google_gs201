408c702680da146feeb6bc7f71e52ff6bcad6833
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-10 09:03:26 +0800

    gs201: disable watchdog timers
    Change-Id: Ia67e83c5eec51de83db59d88bcb6223a738680cd

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -52,6 +52,7 @@ BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem
 BOARD_KERNEL_CMDLINE += disable_dma32=on
 BOARD_KERNEL_CMDLINE += irqaffinity=0-3
 BOARD_KERNEL_CMDLINE += mitigations=off
+BOARD_KERNEL_CMDLINE += nowatchdog nmi_watchdog=0
 BOARD_BOOTCONFIG += androidboot.boot_devices=14700000.ufs
 
 TARGET_NO_BOOTLOADER := true
